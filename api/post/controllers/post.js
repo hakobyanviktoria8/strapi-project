eb620b4118d711e6ecc60669787660b52413415d
Viktorya Hakobyan <hakobyanviktorya8@gmail.com>
2021-06-08 00:01:31 +0400

    Strapi genereted posts

--- api/post/controllers/post.js
+++ api/post/controllers/post.js
@@ -5,16 +5,36 @@
  * to customize this controller
  */
 
+const { parseMultipartData, sanitizeEntity } = require('strapi-utils');
+
 module.exports = {
+  /**
+   * Create a record.
+   *
+   * @return {Object}
+   */
   async create(ctx) {
     let entity;
-    const user = ctx.state.user.id;
     if (ctx.is('multipart')) {
-      const {data, files} = parseMultipartData(ctx);
-      entity = await strapi.services.post.create({...data, user}, {files});
+      const { data, files } = parseMultipartData(ctx);
+      entity = await strapi.services.post.create(data, { files });
     } else {
-      entity = await strapi.services.post.create({...ctx.request.body, user});
+      entity = await strapi.services.post.create(ctx.request.body);
     }
-    return sanitizeEntity(entity, {model: strapi.models.post});
+    return sanitizeEntity(entity, { model: strapi.models.post });
   },
+
+  //don't working right
+
+  // async create(ctx) {
+  //   let entity;
+  //   const user = ctx.state.user.id;
+  //   if (ctx.is('multipart')) {
+  //     const { data, files } = parseMultipartData(ctx);
+  //     entity = await strapi.services.post.create({...data, user}, { files });
+  //   } else {
+  //     entity = await strapi.services.post.create({...ctx.request.body,user});
+  //   }
+  //   return sanitizeEntity(entity, { model: strapi.models.post });
+  // },
 };
