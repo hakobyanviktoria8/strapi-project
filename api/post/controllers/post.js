7697510e3926f727f909170f6d0e967f6e8dfca8
Viktorya Hakobyan <hakobyanviktorya8@gmail.com>
2021-06-07 23:09:45 +0400

    Initial commit

---
+++ api/post/controllers/post.js
@@ -0,0 +1,20 @@
+'use strict';
+
+/**
+ * Read the documentation (https://strapi.io/documentation/developer-docs/latest/development/backend-customization.html#core-controllers)
+ * to customize this controller
+ */
+
+module.exports = {
+  async create(ctx) {
+    let entity;
+    const user=ctx.state.user.id;
+    if (ctx.is('multipart')) {
+      const { data, files } = parseMultipartData(ctx);
+      entity = await strapi.services.posts.create({...data, user}, { files });
+    } else {
+      entity = await strapi.services.posts.create({...ctx.request.body,user});
+    }
+    return sanitizeEntity(entity, { model: strapi.models.posts });
+  },
+};
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || 'f7cc837f-b24f-49dc-840d-9dfbd828349e'
+};
